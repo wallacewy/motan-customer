079e19bf0d036fe22ec43c16a0193ad92aee85b7
Wallace Wan <wallace.wan@sap.com>
2016-11-03 18:50:56 +0800

    Initial project

---
+++ src/main/java/com/wallace/motan/CustomerServiceImpl.java
@@ -0,0 +1,9 @@
+package com.wallace.motan;
+
+public class CustomerServiceImpl implements CustomerService {
+	
+	public String hello(String name) {
+        System.out.println(name + " invoked customer service");
+        return "Hello " + name + ", it's greeting from customer service";
+    }
+}
