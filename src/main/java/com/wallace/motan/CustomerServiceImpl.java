0dd52451dc54f1e89e7114a4be9f589d26683afa
Wallace Wan <wallace.wan@sap.com>
2016-11-07 17:08:22 +0800

    Output: group v2

--- src/main/java/com/wallace/motan/CustomerServiceImpl.java
+++ src/main/java/com/wallace/motan/CustomerServiceImpl.java
@@ -4,6 +4,6 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	public String hello(String name) {
         System.out.println(name + " invoked customer service");
-        return "Hello " + name + ", it's greeting from customer service (group v1)";
+        return "Hello " + name + ", it's greeting from customer service (group v2)";
     }
 }
